9b6c16a6ad187632c4313e7b66563153e6ae144a
yule <yule.gpark@gmail.com>
2022-08-19 21:27:26 +0900

    second commit

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -21,6 +21,7 @@ public class Order {
     @JoinColumn(name = "member_id") // FK의 이름을 "member_id"로.
     private Member member;
 
+    @OneToMany(mappedBy = "order")
     private List<OrderItem> orderItems = new ArrayList<>();
 
     private Delivery delivery;
